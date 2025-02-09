011cd00c707f9fa95a9738e48ff81017b0133773
Felicia Bacon <felibacon@gmail.com>
2025-02-08 19:59:47 -0600

    adding data types exercises

---
+++ books/intro-to-programming-python/exercises/data-types.py
@@ -0,0 +1,22 @@
+# 'True' -> string
+# False -> bool
+# (1, 2, 3) -> tuple
+# 1.5 float
+# [1, 2, 3] -> list
+# 2 -> integer
+# range(5) -> range
+# {1, 2, 3} -> set
+# None -> NoneType
+# {'foo': 'bar'} -> dictionary
+
+# 2. 
+names = ('Asta', 'Butterscotch', 'Pudding', 'Neptune', 'Darwin')
+
+#3. 
+pets = {
+    'Asta': 'dog',
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard'
+}
