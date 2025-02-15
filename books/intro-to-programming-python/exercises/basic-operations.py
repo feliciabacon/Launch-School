d71e2cc099958c8e4a4f59d67a14e5eeb1b1502d
Felicia Bacon <felibacon@gmail.com>
2025-02-15 14:19:54 -0600

    adding basic operations exercises

---
+++ books/intro-to-programming-python/exercises/basic-operations.py
@@ -0,0 +1,38 @@
+# 1. 
+name = "Felicia" + " " + "Bacon"
+print(name)
+
+#2. 
+number = 4936
+
+ones = number % 10
+
+number = number // 10
+
+tens = number % 10
+
+number = number // 10
+
+hundreds = number % 10 
+
+number = number // 10
+
+thousands = number % 10
+
+print(ones, tens, hundreds, thousands)
+
+# 3.
+print(int('5') + int('10')) # '150' -> string concatenation
+
+# 5.
+# foo = ['a', 'b', 'c']
+# print(foo[3])
+
+# 6.
+print('foo' == 'Foo') # False
+
+# 7.
+# print(int('3.1415')) # Error
+
+# 8.
+print('12' < '9') # True
